1f5a04df65fa10a63680f1da2a9bc2765b619c63
satpalaalaria <satpal19@navgurukul.org>
2022-04-28 12:37:41 +0530

    remove cart commit

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 "use strict";
-fetch('https://fakestoreapi.com/products').then(response=>{
+async function asyncall(){
+await fetch('https://fakestoreapi.com/products').then(response=>{
     return response.json();
 }).then(data=>{
     // console.log(data);
-    const products=data.map(product =>{
+    data.map(product =>{
         const cards=`
             <div class="card">
-                <img src="${product.image}" alt="image" style="width:60%; margin:auto; height:65%;"><br>
+                <img src="${product.image}" alt="image" style="width:65%; margin:auto; height:35%;"><br>
                 <h3 style="font-size: 13px;color: rgb(35, 35, 194);">${product.title}</h3>
                 <p class="title" style="font-size: 12px;">${product.category}</p>
                 <h5 style="color: red;">Price:${product.price} &#8377</h5>
@@ -19,9 +20,9 @@ fetch('https://fakestoreapi.com/products').then(response=>{
         const selectElement=document.querySelector('.card-deck');
         // console.log(selectElement);
         selectElement.insertAdjacentHTML('beforeend',cards)
-    
     });
     var svg1=document.querySelector('h1');
+    var select=document.querySelector('.select');
     // console.log(svg1);
     var button=document.getElementsByTagName('button');
     // console.log(button);
@@ -32,11 +33,40 @@ fetch('https://fakestoreapi.com/products').then(response=>{
             var add=Number(svg1.getAttribute('data-count')|| 0);
             svg1.setAttribute('data-count', add + 1);
             svg1.classList.add('zero');
+
+ // move item to cart
+            var parent=e.target.parentNode;
+            var clone=parent.cloneNode(true);
+            select.appendChild(clone);
+            clone.lastElementChild.innerText="Remove"
+            if (clone){
+                svg1.onclick=()=>{
+                    select.classList.toggle('display');
+                }
+            }
+            const removeButton=document.querySelector('.select button');
+            removeButton. onclick = ()=> {
+               select.removeChild(clone) 
+               svg1.setAttribute('data-count', add+1-1);
+               // console. log(event);
+            };
+            // console.log(removeButton);
+            // function remove(){
+            //     const removeButton=document.querySelector('.select button');
+            //     console.log(removeButton);
+            // }
+            // remove();           
         })
     };
+    
 }).catch(error=>{
     console.log(error);
-});
+})};
+asyncall();
+
+
+
+
 
 
 
